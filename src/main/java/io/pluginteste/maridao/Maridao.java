83f8f9339df6da0ad1a7f78a4b93ba36e5342c58
kaitoslr <ramonsousadelima670@gmail.com>
2025-03-22 20:22:40 -0300

    segundo commit, comando hello

--- src/main/java/io/pluginteste/maridao/Maridao.java
+++ src/main/java/io/pluginteste/maridao/Maridao.java
@@ -1,17 +1,35 @@
 package io.pluginteste.maridao;
 
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Maridao extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        // Este método é chamado quando o plugin é carregado
+        getLogger().info("MeuPluginPaper foi habilitado!");
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        // Este método é chamado quando o plugin é desativado
+        getLogger().info("MeuPluginPaper foi desabilitado!");
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (command.getName().equalsIgnoreCase("hello")) {
+            if (sender instanceof Player) {
+                Player player = (Player) sender;
+                player.sendMessage("Olá, " + player.getName() + "!");
+            } else {
+                getLogger().info("Este comando só pode ser usado por um jogador!");
+            }
+            return true;
+        }
+        return false;
     }
 }
