5d73c2f1dc32281add34d64acfa7c12addaa4daf
Willian <brcraftgames@gmail.com>
2025-03-31 22:08:52 -0300

    quadrado do plot e minimo requerido para dar claim

--- src/main/java/Plot/comandoPlot.java
+++ src/main/java/Plot/comandoPlot.java
@@ -6,6 +6,8 @@ import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
 
+import org.bukkit.World;
+import org.bukkit.block.data.BlockData;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -39,25 +41,78 @@ public class comandoPlot implements Listener {
             return;
         }
 
+        if(loc1 != null && loc2 != null){
+            drawSquare(loc1, loc2, player, null);
+        }
+
         event.setCancelled(true);
         switch (event.getAction()){
             case Action.RIGHT_CLICK_BLOCK:
                 if(loc1 != null ) player.sendBlockChange(loc1, loc1.getBlock().getBlockData());
                 loc1 = event.getClickedBlock().getLocation();
                 Bukkit.getScheduler().runTaskLater(JavaPlugin.getPlugin(Maridao.class) , ()->{player.sendBlockChange(loc1, Material.GLOWSTONE.createBlockData());}, 1);
-                player.sendMessage(" §6Local1 " + loc1.getX() + ", " + loc1.getZ());
+                player.sendMessage(" §6Local1 " + loc1.getX() + ", " + loc1.getY() + ", " + loc1.getZ());
                 break;
             case Action.LEFT_CLICK_BLOCK:
                 if(loc2 != null ) player.sendBlockChange(loc2, loc2.getBlock().getBlockData());
                 loc2 = event.getClickedBlock().getLocation();
                 player.sendBlockChange(loc2, Material.GLOWSTONE.createBlockData());
-                player.sendMessage(" §cLocal2 " +  loc2.getX() + ", " + loc2.getZ());
+                player.sendMessage(" §cLocal2 " +  loc2.getX() + ", " + loc2.getY() + ", " + loc2.getZ());
                 break;
+        }
+
+        if(loc1 != null && loc2 != null){
+            drawSquare(loc1, loc2, player, Material.GLOWSTONE.createBlockData());
+        }
+    }
+
+    public static void drawSquare(Location loc1, Location loc2, Player player, BlockData material){
+        int max_x = (int) Math.max(loc1.getX(), loc2.getX());
+        int min_x = (int) Math.min(loc1.getX(), loc2.getX());
+        int max_z = (int) Math.max(loc1.getZ(), loc2.getZ());
+        int min_z = (int) Math.min(loc1.getZ(), loc2.getZ());
+
+        for(int x = min_x; x < max_x; x ++){
+            int low_y_loc1 = player.getWorld().getHighestBlockYAt(x, max_z);
+            int low_y_loc2 = player.getWorld().getHighestBlockYAt(x, min_z);
+            BlockData block_loc1 = material != null ? material : new Location(player.getWorld(), x, low_y_loc1, max_z).getBlock().getBlockData();
+            BlockData block_loc2 = material != null ? material : new Location(player.getWorld(), x, low_y_loc2, max_z).getBlock().getBlockData();
+
+            player.sendBlockChange(new Location(player.getWorld(), x, low_y_loc1 , max_z), block_loc1);
+            player.sendBlockChange(new Location(player.getWorld(), x, low_y_loc2 , min_z), block_loc2);
+        }
+
+        for(int z = min_z; z < max_z; z++){
+            int low_y_loc1 = player.getWorld().getHighestBlockYAt(min_x, z);
+            int low_y_loc2 = player.getWorld().getHighestBlockYAt(max_x, z);
+            BlockData block_loc1 = material != null ? material : new Location(player.getWorld(), min_x, low_y_loc1, z).getBlock().getBlockData();
+            BlockData block_loc2 = material != null ? material : new Location(player.getWorld(), min_x, low_y_loc2, z).getBlock().getBlockData();
+
+            player.sendBlockChange(new Location(player.getWorld(), min_x, low_y_loc1 , z), block_loc1);
+            player.sendBlockChange(new Location(player.getWorld(), max_x, low_y_loc2 , z), block_loc2);
         }
     }
 
     public static void setClaim(Player player){
+        if(!validateMinSize(loc1, loc2)){
+            player.sendMessage("Seu terreno deve ter pelo menos 36 blocos de tamanho !!!");
+            return;
+        }
         savePlotLocation(player.getUniqueId().toString(), loc1.toString(), loc2.toString());
+        player.sendMessage("Terreno assegurado com sucesso !!!");
+        if(loc1 != null && loc2 != null){
+            drawSquare(loc1, loc2, player, Material.DIAMOND_BLOCK.createBlockData());
+        }
+        loc2 = null;
+        loc1 = null;
+    }
+
+    public static boolean validateMinSize(Location loc1, Location loc2) {
+        double diff_x = Math.abs(loc1.getX() - loc2.getX());
+        double diff_z = Math.abs(loc1.getZ() - loc2.getZ());
+        double area = diff_x * diff_z;
+
+        return area > 36;
     }
 
 
